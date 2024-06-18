a77ab7e85834991ded370ff0e6b8295e286b2a5e
Jado49 <mic2016mic2016@gmail.com>
2024-06-18 21:11:51 +0100

    postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+// postcss.config.js
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    cssnano: {
+      preset: 'default',
+    },
+    // other plugins can be added here as needed
+  }
+}
