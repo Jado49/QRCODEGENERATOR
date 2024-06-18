7ad0b4dc382063ac3a1b9438247ccb268d55d9cf
Jado49 <mic2016mic2016@gmail.com>
2024-06-18 21:10:09 +0100

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'qrcody-blue': '#3490dc',
+        'qrcody-yellow': '#f59e0b',
+      },
+      fontFamily: {
+        'qrcody': ['Roboto', 'sans-serif'],
+      },
+      // other customizations as needed
+    },
   },
   variants: {
     extend: {},
